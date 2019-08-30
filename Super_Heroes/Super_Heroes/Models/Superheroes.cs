63ec731f01e48bddcdc2c83a4ea65dfbdf32506b
Charles Clark <ctcb57@gmail.com>
2019-08-30 11:21:59 -0500

    Ensured all pages are bootsrap compliant

--- Super_Heroes/Super_Heroes/Models/Superheroes.cs
+++ Super_Heroes/Super_Heroes/Models/Superheroes.cs
@@ -10,10 +10,15 @@ namespace Super_Heroes.Models
     {
         [Key]
         public int superheroId { get; set; }
+        [Display(Name = "Superhero Name")]
         public string superheroName { get; set; }
+        [Display(Name = "Alter Ego")]
         public string alterEgo { get; set; }
+        [Display(Name = "Primary Ability")]
         public string primaryAbility { get; set; }
+        [Display(Name = "Secondary Ability")]
         public string secondaryAbility { get; set; }
+        [Display(Name = "Catchphrase")]
         public string catchphrase { get; set; }
     }
 }
